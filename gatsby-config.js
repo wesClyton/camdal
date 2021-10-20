c4c0b46fc6000819e2d0a2960a3b7e8772b73ab4
Felipe Mantovani <felipexmantovani@gmail.com>
2021-10-20 19:46:44 -0300

    Finaliza header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,29 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Camdal",
+    siteUrl: 'https://camdal.com.br/',
+    title: 'Camdal',
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Camdal',
+        short_name: 'Camdal',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        display: 'standalone',
+        icon: 'src/images/logo-favicon.png',
+        crossOrigin: 'use-credentials',
+      },
+    },
+  ],
 };

a6beb585177e7cfe9302e927a873965f097d46e9
Weslley Hinsch <wes.hinsch@gmail.com>
2025-04-10 12:55:37 -0300

    configuracao inicial astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Certifique-se de incluir os arquivos Astro
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

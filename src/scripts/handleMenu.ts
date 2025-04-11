ed8e33bb20656b410e318ab6c7a07ba7c701ad8f
Weslley Hinsch <wes.hinsch@gmail.com>
2025-04-11 20:17:44 -0300

    Menu responsivo

---
+++ src/scripts/handleMenu.ts
@@ -0,0 +1,20 @@
+function initializeMenuHandler() {
+  const buttonMenu = document.getElementById("activeMenu");
+
+  buttonMenu?.addEventListener("click", () => {
+    console.log(`nois`);
+
+    const menu = document.getElementById("navbar-cta");
+    const isActive = menu?.classList.contains("hidden");
+    menu?.classList.toggle("hidden", !isActive);
+    buttonMenu.setAttribute("aria-expanded", String(!isActive));
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initializeMenuHandler();
+});
+
+document.addEventListener("astro:after-swap", () => {
+  initializeMenuHandler();
+});
--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Certifique-se de incluir os arquivos Astro
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};

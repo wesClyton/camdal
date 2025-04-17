f5ba26c035f96eb9f9d39685cc9efd1b706583a4
Weslley Hinsch <wes.hinsch@gmail.com>
2025-04-17 10:58:49 -0300

    adicionado plugin vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 import sitemapPlugin from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -7,6 +8,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
   vite: {
     plugins: [tailwindcss()],
   },

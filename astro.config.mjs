ea734458cb7adae46c5f4cd29ef4ae077fd172d8
Weslley Hinsch <wes.hinsch@gmail.com>
2025-04-17 11:48:47 -0300

    section sobre camdal

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   adapter: vercel({
     edgeMiddleware: true,
